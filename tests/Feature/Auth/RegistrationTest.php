fd74522e3356fc087782b9c381f9d39d51ef4548
Ettienne Botha <ettienne.botha@susodigital.com>
2025-10-29 13:12:10 +0200

    fix test_new_users_can_register

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -22,7 +22,7 @@ class RegistrationTest extends TestCase
         $response = Volt::test('auth.register')
             ->set('name', 'Test User')
             ->set('email', 'test@example.com')
-            ->set('cellphone', '0810000000')
+            ->set('cellphone', '+264810000000')
             ->set('password', 'password')
             ->set('password_confirmation', 'password')
             ->call('register');
